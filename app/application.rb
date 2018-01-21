be6998deba8e2e9e96368aa57437681e301d6073
Elle Faraday <faraday.elle@gmail.com>
2018-01-21 00:26:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,8 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-
-    if Time.now > 12
+     time = Time.now
+    if time.houe < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
